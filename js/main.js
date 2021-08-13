0e34ad380c70d742c55ececdcec698e09f57c213
Tom <tomleesm@gmail.com>
2021-08-13 17:20:18 +0800

    add uuidv4.js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const app = Vue.createApp({
+  data() {
+    return {
+      taskListNotDone: []
+    }
+  },
+  methods: {
+    addTask() {
+      let newTask = {
+        // 標題、詳細資料和日期 / 時間
+        id: 'T001',// id for :key
+        title: '',
+        detail: '',
+        dateTime: null
+      }
+      this.taskListNotDone.push(newTask)
+    }
+  }
+})
+
+app.mount('#app')
